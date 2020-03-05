3e0bd3585faf4ac46bfe688741b83144316f028d
Thijs Kraakman <thijskraakman@outlook.com>
2020-03-05 15:00:45 +0100

    .

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/imac/code/blaze-app/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/imac/code/blaze-app/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/imac/code/soundboard-app/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/imac/code/soundboard-app/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
